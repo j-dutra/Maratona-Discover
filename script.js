947bbb1bb9aa14e1a3960941f2ff5b537512f4a9
João L. Dutra <jdutra.eu@gmail.com>
2021-01-31 23:50:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,78 @@
-const Modal = {
+const Modal = { // mudar função toggle
     open(){
         // Abrir Modal
         // Adicionar a class active ao modal
-        document.querySelector(".modal-overlay")
-        .classList.add('active')
+
+        document.
+            querySelector(".modal-overlay")
+            .classList
+            .add('active')
     },
     close(){
         // Fechar o modal
-        // remover a classe active do modal
+        // Remover a classe active do modal
+
         document.querySelector(".modal-overlay")
-        .classList.remove('active')
+        .classList
+        .remove('active')
+    }
+}
+
+const transactions = [
+    {
+        id: 1,
+        description: "luz"
+        amount: -50000,
+        date: "23/01/2021"
+    },
+    {
+        id: 2,
+        description: "Criação Website"
+        amount: 500000,
+        date: "23/01/2021"
+    },
+    {
+
+        id: 3,
+        description: "Internet"
+        amount: -20000,
+        date: "23/01/2021"
+    }
+]
+
+const Transaction = {
+    incomes(){
+        // somar as entradas
+    }
+    expanse(){
+        // Calcular as saidas
+    }
+    total(){
+        // Entradas - saidas
+    }
+}
+
+// preciso substituir os dados do HTML como os dados do Js
+// pegar as minhas transações do meu 
+// objeto aqui no javascript
+// e colocar no lá no HTML
+
+const DOM = {
+    addTransaction(transaction, index){
+        const tr = document.createElement("tr")
+    },
+
+
+    innerHTMLTransaction(){
+        const html = `
+    
+        <td class="description">Luz</td>
+        <td class="expanse">- R$ 500,00</td>
+        <td class="date">23/01/2021</td>
+        <td>
+            <img src="./assets/minus.svg" alt="Remover Transação">
+        </td>
+        
+        `
     }
 }
